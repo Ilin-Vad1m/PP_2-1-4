834b3c23fe2ff37aecea4607a3e97894dbf7f221
Vadim Ilin <vad2.com@gmail.com>
2022-03-03 22:26:12 +0300

    Death Koschei

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.KoscheiTheDeathless;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +11,4 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
-    public static Island2 getIsland(Wood3 wood) {
-        return new Island2(wood);
-    }
 }
